00667415655dbb95f938e567ec475f2d65ff219e
demid-ns <siwi1992@gmail.com>
2021-06-03 11:25:35 +0300

    Production env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  resApi: 'https://demid13th.site',
+  GOOGLE_CLIENT_ID: '651324120148-b7krj29ds89vv93n73fpt7hgtcen4llr.apps.googleusercontent.com',
+  FACEBOOK_CLIENT_ID: '2550684838572555',
+  tokenAllowedDomains: ['demid13th.site']
 };
